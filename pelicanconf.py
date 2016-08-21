20c1b5ceaea24480dc9c5ae1195ad4992d033c8c
chris <chrisvmiller@gmail.com>
2016-08-21 13:43:49 -0700

    update and cleanup deploy

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
 THEME = u'cleanliness'
 
 AUTHOR = u'Chris Miller'
@@ -12,30 +8,22 @@ TIMEZONE = u'US/Pacific'
 
 DEFAULT_LANG = u'en'
 
-# Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 DISPLAY_PAGES_ON_MENU = True
 
-# Social widget
-SOCIAL = (
-    (u'github-square', u'https://github.com/chrisvmiller'),
-)
-
 DEFAULT_PAGINATION = 5
 
 RELATIVE_URLS = True
 
 PATH = u'content'
-# ARTICLE_EXCLUDES = (('pages'))
 
-PROFILE_IMAGE_URL = u''
 STATIC_PATHS = [u'assets']
 
 ARTICLE_URL = u'{date:%Y}/{slug}/'
 ARTICLE_SAVE_AS = u'{date:%Y}/{slug}/index.html'
 
 PLUGIN_PATHS = ['cleanliness/plugins']
-PLUGINS = ['urlabsolutifier']
+PLUGINS = []
