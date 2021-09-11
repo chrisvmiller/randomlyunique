9d315d1c0f3a945d3a2e7cfb599e851c97e7d5a8
Chris Miller <chrisvmiller@gmail.com>
2021-09-10 20:32:06 -0700

    fix broken stylesheets

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,9 +11,6 @@ FEED_MAX_ITEMS = 10
 
 PATH = u'content'
 STATIC_PATHS = [u'assets']
-EXTRA_PATH_METADATA = {
-    'assets/common/favicon.ico': {'path': 'favicon.ico'}
-}
 
 ARTICLE_URL = u'{category}/{date:%Y}/{slug}/index.html'
 ARTICLE_SAVE_AS = u'{category}/{date:%Y}/{slug}/index.html'
